ba2992130ef752c63164f8c7772e573a37f2fb65
Dan Stroot <dan.stroot@gmail.com>
2019-02-10 16:13:36 -0800

    working checkpoint

--- src/components/HeroBlue/index.test.js
+++ src/components/HeroBlue/index.test.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import React from "react";
+import renderer from "react-test-renderer";
 
-import HeroBlue from '../HeroBlue';
-import image from '../../components/HeroBlue/media/startup.jpg';
+import HeroBlue from "../HeroBlue";
+import image from "../../components/HeroBlue/media/startup.jpg";
 
-test('HeroBlue renders some content', () => {
-  const component = renderer.create(<HeroBlue image={image} />);
+test("HeroBlue renders some content", () => {
+  const component = renderer.create(
+    <HeroBlue image={image} tagline="It's Miller Time!" />
+  );
 
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
--- src/components/Map/index.test.js
+++ src/components/Map/index.test.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Map from '../Map';
+import React from "react";
+import renderer from "react-test-renderer";
+import Map from "../Map";
 
 // Some default styles
 const styles = {
-  width: '100%',
-  height: '500px',
+  width: "100%",
+  height: "500px"
 };
 
-const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+// const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+const API_KEY = "mykey";
 
-describe('Map', () => {
-  it('it should render', () => {
+describe("Map", () => {
+  it("it should render", () => {
     const component = renderer.create(
       <Map styles={styles} APIKey={API_KEY} scheme="dark" />
     );
