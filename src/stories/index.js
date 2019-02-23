7912d8cfd743aa8cb8f8d771e374aa31eee1cbcb
Dan Stroot <dan.stroot@gmail.com>
2019-02-23 07:16:01 -0800

    remove top margin

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
+import React from 'react';
+import { storiesOf } from '@storybook/react';
 
 // import { action } from '@storybook/addon-actions';
 // import { linkTo } from '@storybook/addon-links';
@@ -8,16 +8,16 @@ import { storiesOf } from "@storybook/react";
 // https://fontawesome.com/icons?d=gallery
 // https://github.com/FortAwesome/react-fontawesome
 // https://fontawesome.com/how-to-use/on-the-web/using-with/react
-import { library } from "@fortawesome/fontawesome-svg-core";
+import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faSpinner,
   faCheck,
   faCheckCircle,
-  faExclamationTriangle
-} from "@fortawesome/free-solid-svg-icons";
+  faExclamationTriangle,
+} from '@fortawesome/free-solid-svg-icons';
 
 // our main css
-import "../scss/main.scss";
+import '../scss/main.scss';
 
 // library of Font Awesome Icons
 library.add(faSpinner, faCheck, faCheckCircle, faExclamationTriangle);
@@ -26,14 +26,14 @@ library.add(faSpinner, faCheck, faCheckCircle, faExclamationTriangle);
  * STORIES
  */
 
-storiesOf("Welcome", module)
+storiesOf('Welcome', module)
   .addParameters({
     info: {
-      disable: true
-    }
+      disable: true,
+    },
   })
-  .add("to Storybook", () => (
+  .add('to Storybook', () => (
     <div>
-      <h1 className="display-4">Welcome to our storybook!</h1>
+      <h1 className="display-4 text-center">Welcome to our storybook!</h1>
     </div>
   ));
