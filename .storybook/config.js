d0f40edfa1c809d2cedbac63393cd2a2190b36dc
Dan Stroot <dan.stroot@gmail.com>
2019-02-23 06:58:51 -0800

    added readme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,21 +1,32 @@
-import { addDecorator, configure } from "@storybook/react";
-import { withOptions } from "@storybook/addon-options";
-import { withInfo } from "@storybook/addon-info";
-import { checkA11y } from "@storybook/addon-a11y";
+import { addDecorator, configure } from '@storybook/react';
+import { withOptions } from '@storybook/addon-options';
+import { withInfo } from '@storybook/addon-info';
+import { checkA11y } from '@storybook/addon-a11y';
 
 // Get stories
-const req = require.context("../src/components", true, /\.stories\.js$/);
+const req = require.context('../src/components', true, /\.stories\.js$/);
 function loadStories() {
-  require("../src/stories"); // from setup
+  require('../src/stories'); // from setup
   req.keys().forEach(filename => req(filename)); // from components
 }
 
 // NOTE: It is important to declare "withInfo" as the first decorator
-//       https://github.com/storybooks/storybook/tree/master/addons/info
+//       (https://github.com/storybooks/storybook/tree/master/addons/info)
 
 // Global decorators
 addDecorator(withInfo());
 addDecorator(checkA11y);
-addDecorator(withOptions({ name: "Our Library" }));
+addDecorator(
+  withOptions({
+    name: 'Our Library',
+    // url: 'https://test.com',
+    // goFullScreen: false,
+    // showLeftPanel: true,
+    // showDownPanel: true,
+    // showSearchBox: false,
+    // downPanelInRight: true,
+    // sortStoriesByKind: true,
+  })
+);
 
 configure(loadStories, module);
---
+++ README.md
@@ -0,0 +1,34 @@
+# React Component Library
+
+This is an example of a Storybook library showing how a library of components would be maintained. I use this readme as a scrtach pad to keep track of certain things and for reminders of small tweaks.
+
+#### Adding SVGs
+
+One way to add SVG files was described in the section above. However react-scripts@2.0.0 added an ability to import SVGs as React components through the fantastic @svgr/webpack. You can use either of the two approaches. In your code it would look like:
+
+```js
+import logoUrl, { ReactComponent as Logo } from './logo.svg';
+
+console.log(logoUrl); // /logo.84287d09.svg
+
+const App = () => (
+  <div>
+    {/* logoUrl is the URL of your SVG file */}
+    <img src={logoUrl} alt="Logo" />
+
+    {/* Logo is an actual React component */}
+    <Logo />
+  </div>
+);
+```
+
+## IE 11 support :(
+
+See: https://github.com/facebook/create-react-app/tree/master/packages/react-app-polyfill
+
+top level index.js:
+
+```js
+// This must be the first line in src/index.js
+import 'react-app-polyfill/ie11';
+```
