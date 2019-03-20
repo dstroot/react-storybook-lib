71e3652752693c143549596f04e4007395a1ffd7
Dan Stroot <dan.stroot@gmail.com>
2019-03-20 08:46:14 -0700

    dependencies

---
+++ src/components/Routes/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Router } from "@reach/router";
+
+// create a component that wraps a component and adds the path property
+const Route = ({ component: Component, ...rest }) => <Component {...rest} />;
+
+// Routes component that provides the Router and Routes
+const Routes = ({ data }) => (
+  <Router>
+    {data.map((item, index) => (
+      <Route key={index} path={item.path} component={item.component} />
+    ))}
+  </Router>
+);
+
+export default Routes;
+
+/**
+ * Below is how this is used
+ */
+
+//  // components
+// import NavBar from "./components/NavBar";
+// import Footer from "./components/Footer";
+// import Routes from "./Routes";
+
+// // pages
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+
+// const navData = [
+//   { path: "/", name: "Home", component: Home },
+//   { path: "/about", name: "About", component: About }
+// ];
+
+// const App = () => (
+//   <>
+//     <NavBar data={navData} />
+//     <Routes data={navData} />
+//     <Footer />
+//   </>
+// );
