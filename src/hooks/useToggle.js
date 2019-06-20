f002f60a4226d2410f6f7019835e6322bc11b6d2
Dan Stroot <dan.stroot@gmail.com>
2019-06-19 17:33:44 -0700

    more hooks

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,6 +1,19 @@
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 
-export const useToggle = initial => {
-  const [open, setOpen] = useState(initial);
-  return [open, useCallback(() => setOpen(status => !status))];
+// export const useToggle = initial => {
+//   const [open, setOpen] = useState(initial);
+//   return [open, useCallback(() => setOpen(status => !status))];
+// };
+
+export const useToggle = initialValue => {
+  const [value, setValue] = useState(initialValue);
+  const toggleValue = () => setValue(!value);
+  return [value, toggleValue];
+};
+
+export const useBoolean = initialValue => {
+  const [value, setValue] = useState(initialValue);
+  const setTrue = () => setValue(true);
+  const setFalse = () => setValue(false);
+  return [value, setTrue, setFalse];
 };
